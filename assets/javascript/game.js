139f8e3553dbe1c144f945e15bfbbdb6db2ad5c4
Nicholas Ready <Ready.nicholas@gmail.com>
2019-09-10 18:13:04 -0500

    js file with resets and correct if statements on first click event

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,28 +19,36 @@ $(document).ready(function () {
     var totalScore = 0;
     $("#totalScore").text(totalScore);
 
-    
+    function win(){
+        wins++;
+        $("#wins").text(wins);
+        reset();
+    }
+
+    function lose(){
+        losses++;
+        $("#losses").text(losses);
+        reset();
+    }
 
     $("#crystal1").on("click", function () {
         totalScore = totalScore + crystal1;
         $("#totalScore").text(totalScore);
         if(totalScore == randomNumber){
-            wins++; 
-            $("#wins").text(wins);
-        totalScore=0;
-    }
-            //add code: randomize numbers again for a start of a new game
-            else if(totalScore>randomNumber){
-            losses++;
-            $("#losses").text(losses)
-            //add code: randomize number again for a start of a new game.
-            }
+            win();
+            reset();
+    } else if (totalScore > randomNumber) {
+        lose();
+        reset();}
+        
         });
     
 
     $("#crystal2").on("click", function () {
         totalScore = totalScore + crystal2;
         $("#totalScore").text(totalScore);
+         
+        
     });
 
     $("#crystal3").on("click", function () {
@@ -52,6 +60,17 @@ $(document).ready(function () {
         totalScore = totalScore + crystal4;
         $("#totalScore").text(totalScore);
     })
+
+function reset(){
+    totalScore=0;
+    $("#totalScore").text(totalScore);
+    randomNumber = Math.floor(Math.random() * 101) + 19;
+    $("#randomNumberField").html(randomNumber);
+    crystal1 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+ crystal2 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+    crystal3 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+    crystal4 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+}
 
 
 
