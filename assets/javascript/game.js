d54d034c3bfcceffe0b2c206804265e5731ab915
Nicholas Ready <Ready.nicholas@gmail.com>
2019-09-10 19:51:14 -0500

    Crystal game with finished if-statements and comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,65 +12,94 @@ $(document).ready(function () {
     var crystal2 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
     var crystal3 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
     var crystal4 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
-
+ 
+    //set initial scores to 0
     var wins = 0;
     var losses = 0;
 
+    //set the Total Score to zero and add it to the page using the JQuery syntax
     var totalScore = 0;
     $("#totalScore").text(totalScore);
 
-    function win(){
+    //Create a function called "win" that adds a point to win, adds it to its respective field, and then resets the game with new values
+    function win() {
         wins++;
         $("#wins").text(wins);
         reset();
     }
-
-    function lose(){
+//Create a function called "lose" that addes a point to the losses field, then adds it to its respective field, and resets.
+    function lose() {
         losses++;
         $("#losses").text(losses);
         reset();
     }
 
+    //Create a click event for the crystals that adds its random value to the TotalScore. If the click event makes the RandomNumber = totalScore then run the win and reset functions
     $("#crystal1").on("click", function () {
         totalScore = totalScore + crystal1;
         $("#totalScore").text(totalScore);
-        if(totalScore == randomNumber){
+        if (totalScore == randomNumber) {
             win();
             reset();
-    } else if (totalScore > randomNumber) {
-        lose();
-        reset();}
-        
-        });
-    
+        } else if (totalScore > randomNumber) {
+            lose();
+            reset();
+        }
+
+    });
+
 
     $("#crystal2").on("click", function () {
         totalScore = totalScore + crystal2;
         $("#totalScore").text(totalScore);
-         
-        
+        if (totalScore == randomNumber) {
+            win();
+            reset();
+        } else if (totalScore > randomNumber) {
+            lose();
+            reset();
+        }
+
+
     });
 
     $("#crystal3").on("click", function () {
         totalScore = totalScore + crystal3;
         $("#totalScore").text(totalScore);
+        if (totalScore == randomNumber) {
+            win();
+            reset();
+        } else if (totalScore > randomNumber) {
+            lose();
+            reset();
+        }
+
     });
-    
+
     $("#crystal4").on("click", function () {
         totalScore = totalScore + crystal4;
         $("#totalScore").text(totalScore);
+        if (totalScore == randomNumber) {
+            win();
+            reset();
+        } else if (totalScore > randomNumber) {
+            lose();
+            reset();
+        }
+
     })
 
-function reset(){
-    totalScore=0;
-    $("#totalScore").text(totalScore);
-    randomNumber = Math.floor(Math.random() * 101) + 19;
-    $("#randomNumberField").html(randomNumber);
-    crystal1 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
- crystal2 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
-    crystal3 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
-    crystal4 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
-}
+    //Create one large reset function that sets the TotalScore to 0, and randomizes the crystal values and randomNumber.
+    function reset() {
+        totalScore = 0;
+        $("#totalScore").text(totalScore);
+        randomNumber = Math.floor(Math.random() * 101) + 19;
+        $("#randomNumberField").html(randomNumber);
+        crystal1 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+        crystal2 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+        crystal3 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+        crystal4 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+    }
 
 
 
